d1479cbccdcbb0ebe5e854e89311e16d114ad153
Antu Saha <antusaha970@gmail.com>
2025-01-28 11:55:48 +0600

    added all codes in try except

--- member/views.py
+++ member/views.py
@@ -13,65 +13,70 @@ class MemberView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
+        try:
+            data = request.data
+            member_serializer = serializers.MemberSerializer(data=data)
+            member_financial_basics_serializer = serializers.MembersFinancialBasicsSerializer(
+                data=data)
+            is_member_serializer_valid = member_serializer.is_valid()
+            is_member_financial_serializer_valid = member_financial_basics_serializer.is_valid()
+            if is_member_serializer_valid and is_member_financial_serializer_valid:
+                with transaction.atomic():
+                    member = member_serializer.save()
+                    member_financial_basics_serializer.save(
+                        member_ID=member.member_ID)
 
-        data = request.data
-        member_serializer = serializers.MemberSerializer(data=data)
-        member_financial_basics_serializer = serializers.MembersFinancialBasicsSerializer(
-            data=data)
-        is_member_serializer_valid = member_serializer.is_valid()
-        is_member_financial_serializer_valid = member_financial_basics_serializer.is_valid()
-        if is_member_serializer_valid and is_member_financial_serializer_valid:
-            with transaction.atomic():
-                member = member_serializer.save()
-                member_financial_basics_serializer.save(
-                    member_ID=member.member_ID)
-
+                    return Response({
+                        'data': {
+                            'member_ID': member.member_ID,
+                        },
+                        'status': 'created'
+                    }, status=status.HTTP_201_CREATED)
+            else:
+                # Merge errors from both serializers
+                merged_errors = {**member_serializer.errors, **
+                                 member_financial_basics_serializer.errors}
                 return Response({
-                    'data': {
-                        'member_ID': member.member_ID,
-                    },
-                    'status': 'created'
-                }, status=status.HTTP_201_CREATED)
-        else:
-            # Merge errors from both serializers
-            merged_errors = {**member_serializer.errors, **
-                             member_financial_basics_serializer.errors}
-            return Response({
-                "errors": merged_errors,
-                "status": "failed"
-            }, status=status.HTTP_400_BAD_REQUEST)
+                    "errors": merged_errors,
+                    "status": "failed"
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as server_error:
+            return Response({'detail': "Internal Server Error", 'error_message': str(server_error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def patch(self, request, member_id):
         member = get_object_or_404(Member, member_ID=member_id)
         member_financial_basics = get_object_or_404(
             MembersFinancialBasics, member=member)
-        data = request.data
-        member_serializer = serializers.MemberSerializer(member, data=data)
-        member_financial_basics_serializer = serializers.MembersFinancialBasicsSerializer(
-            member_financial_basics,
-            data=data)
-        is_member_serializer_valid = member_serializer.is_valid()
-        is_member_financial_serializer_valid = member_financial_basics_serializer.is_valid()
-        if is_member_serializer_valid and is_member_financial_serializer_valid:
-            with transaction.atomic():
-                member = member_serializer.save()
-                member_financial_basics_serializer.save(
-                    member_ID=member.member_ID)
+        try:
+            data = request.data
+            member_serializer = serializers.MemberSerializer(member, data=data)
+            member_financial_basics_serializer = serializers.MembersFinancialBasicsSerializer(
+                member_financial_basics,
+                data=data)
+            is_member_serializer_valid = member_serializer.is_valid()
+            is_member_financial_serializer_valid = member_financial_basics_serializer.is_valid()
+            if is_member_serializer_valid and is_member_financial_serializer_valid:
+                with transaction.atomic():
+                    member = member_serializer.save()
+                    member_financial_basics_serializer.save(
+                        member_ID=member.member_ID)
 
+                    return Response({
+                        'data': {
+                            'member_ID': member.member_ID,
+                        },
+                        'status': 'updated'
+                    }, status=status.HTTP_200_OK)
+            else:
+                # Merge errors from both serializers
+                merged_errors = {**member_serializer.errors, **
+                                 member_financial_basics_serializer.errors}
                 return Response({
-                    'data': {
-                        'member_ID': member.member_ID,
-                    },
-                    'status': 'updated'
-                }, status=status.HTTP_200_OK)
-        else:
-            # Merge errors from both serializers
-            merged_errors = {**member_serializer.errors, **
-                             member_financial_basics_serializer.errors}
-            return Response({
-                "errors": merged_errors,
-                "status": "failed"
-            }, status=status.HTTP_400_BAD_REQUEST)
+                    "errors": merged_errors,
+                    "status": "failed"
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as server_error:
+            return Response({'detail': "Internal Server Error", 'error_message': str(server_error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def delete(self, request, member_id):
         member = get_object_or_404(Member, member_ID=member_id)
@@ -99,15 +104,18 @@ class MemberIdView(APIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request):
-        data = request.data
-        serializer = serializers.MemberIdSerializer(data=data)
-        if serializer.is_valid():
-            membership_type = serializer.validated_data['membership_type']
-            id = generate_member_id(membership_type)
-            return Response({
-                'new_generated_id': id
-            }, status=status.HTTP_200_OK)
-        else:
-            return Response({
-                'errors': serializer.errors
-            }, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            data = request.data
+            serializer = serializers.MemberIdSerializer(data=data)
+            if serializer.is_valid():
+                membership_type = serializer.validated_data['membership_type']
+                id = generate_member_id(membership_type)
+                return Response({
+                    'new_generated_id': id
+                }, status=status.HTTP_200_OK)
+            else:
+                return Response({
+                    'errors': serializer.errors
+                }, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as server_error:
+            return Response({'detail': "Internal Server Error", 'error_message': str(server_error)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
