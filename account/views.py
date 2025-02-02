3561dc8ccd94bb7b73cbf5698fd2ac4ae85a8e0f
Antu Saha <antusaha970@gmail.com>
2025-02-02 14:54:02 +0600

    fixed try catch error

--- account/views.py
+++ account/views.py
@@ -395,14 +395,17 @@ class AssignGroupPermissionView(APIView):
             }, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request):
-        serializer = DeleteUserFromGroupSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.validated_data['user_id']
-            group = serializer.validated_data['group_id']
-            assign_group = AssignGroupPermission.objects.get(user=user)
-            assign_group.group.remove(group)
-            assign_group.save()
-            clear_user_permissions_cache()
-            return Response({"detail": "User removed from group successfully."}, status=status.HTTP_200_OK)
-        else:
-            return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        try:
+            serializer = DeleteUserFromGroupSerializer(data=request.data)
+            if serializer.is_valid():
+                user = serializer.validated_data['user_id']
+                group = serializer.validated_data['group_id']
+                assign_group = AssignGroupPermission.objects.get(user=user)
+                assign_group.group.remove(group)
+                assign_group.save()
+                clear_user_permissions_cache()
+                return Response({"detail": "User removed from group successfully."}, status=status.HTTP_200_OK)
+            else:
+                return Response({'errors': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'errors': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
