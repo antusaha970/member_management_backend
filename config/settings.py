4d0a8fc9c1baafa8a07263d2e979b40a21ced32d
Antu Saha <antusaha970@gmail.com>
2025-02-11 09:08:15 +0600

    commited changes

--- config/settings.py
+++ config/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'EXCEPTION_HANDLER': 'account.utils.exceptions.custom_exception_handler', 
+    'EXCEPTION_HANDLER': 'account.utils.exceptions.custom_exception_handler',
 }
 
 SIMPLE_JWT = {
