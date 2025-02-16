55abe7e9178e9717f60c902a9e4d7fba4525f2c4
Antu Saha <antusaha970@gmail.com>
2025-02-16 17:32:07 +0600

    Added models in admin file

--- member/admin.py
+++ member/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from .models import Member, MembersFinancialBasics
+from . import models
 
-admin.site.register(Member)
-admin.site.register(MembersFinancialBasics)
+admin.site.register(models.Member)
+admin.site.register(models.MembersFinancialBasics)
+admin.site.register(models.AddressTypeChoice)
+admin.site.register(models.Address)
+admin.site.register(models.DescendantRelationChoice)
+admin.site.register(models.Descendant)
+admin.site.register(models.Certificate)
+admin.site.register(models.ContactTypeChoice)
+admin.site.register(models.ContactNumber)
+admin.site.register(models.Profession)
+admin.site.register(models.CompanionInformation)
+admin.site.register(models.SpecialDay)
+admin.site.register(models.Spouse)
+admin.site.register(models.SpouseStatusChoice)
