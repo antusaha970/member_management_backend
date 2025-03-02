39ad4802f6463db057c4511ab35d359d630b8350
Antu Saha <antusaha970@gmail.com>
2025-03-02 14:03:32 +0600

    fixed celery mail settings

--- config/celery.py
+++ config/celery.py
@@ -4,8 +4,13 @@ import os
 from celery import Celery
 from django.conf import settings
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-
+DJANGO_ENV = os.getenv('DJANGO_ENV', 'development')
+if DJANGO_ENV == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'config.settings.production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'config.settings.development')
 app = Celery('config')
 app.conf.update(timezone='Asia/Dhaka')
 app.config_from_object(settings, namespace='CELERY')
