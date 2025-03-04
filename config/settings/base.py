0230088fdd40bbd20eee266268bc3ed2340099d3
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-03-04 15:36:30 +0600

    undo rest framwork url

--- config/settings/base.py
+++ config/settings/base.py
@@ -85,7 +85,9 @@ AUTH_USER_MODEL = 'account.CustomUser'
 
 # REST Framework
 REST_FRAMEWORK = {
+    
     'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework_simplejwt.authentication.JWTAuthentication'],
+    
     'EXCEPTION_HANDLER': 'account.utils.exceptions.custom_exception_handler',
     'DEFAULT_THROTTLE_CLASSES': [
         'rest_framework.throttling.UserRateThrottle',  # Throttles per user
