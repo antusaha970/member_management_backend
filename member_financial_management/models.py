613868502781f0d5c0fc6708ebe83bad93e21ddf
Antu Saha <antusaha970@gmail.com>
2025-03-25 16:18:49 +0600

    added member financial models

--- member_financial_management/models.py
+++ member_financial_management/models.py
@@ -126,7 +126,87 @@ class Payment(FinancialBaseModel):
         return f"{self.invoice.invoice_number}"
 
 
+class SaleType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Sale(FinancialBaseModel):
     sale_number = models.CharField(max_length=300, unique=True)
     sale_source_id = models.CharField(max_length=10, blank=True, null=True)
     quantity = models.IntegerField()
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
+    sub_total = models.DecimalField(max_digits=10, decimal_places=2)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    tax = models.DecimalField(max_digits=10, decimal_places=2)
+    total_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    payment_status = models.CharField(max_length=150, blank=True, default="")
+    sales_date = models.DateField(auto_now_add=True)
+    due_date = models.DateField(blank=True, null=True, default=None)
+    notes = models.TextField(blank=True, default="")
+    promo_code = models.CharField(max_length=300, blank=True, default="")
+
+    # relation
+    sale_source_type = models.ForeignKey(
+        SaleType, on_delete=models.PROTECT, related_name="sale_source")
+    customer = models.ForeignKey(
+        Member, on_delete=models.PROTECT, related_name="sale_customer")
+    payment_method = models.ForeignKey(
+        PaymentMethod, on_delete=models.PROTECT, related_name="payment_method")
+    invoice = models.ForeignKey(
+        Invoice, on_delete=models.PROTECT, related_name="sale_invoice")
+
+    def __str__(self):
+        return self.sale_number
+
+
+class IncomeParticular(FinancialBaseModel):
+    name = models.CharField(max_length=500, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class IncomeReceivingOption(FinancialBaseModel):
+    name = models.CharField(max_length=500, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class IncomeReceivingType(FinancialBaseModel):
+    name = models.CharField(max_length=500, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Income(FinancialBaseModel):
+    date = models.DateTimeField(auto_now_add=True)
+    receivable_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_applied = models.DecimalField(max_digits=10, decimal_places=2)
+    discount_name = models.CharField(max_length=255, blank=True, default="")
+    discounted_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, default=0)
+    final_receivable = models.DecimalField(max_digits=10, decimal_places=2)
+    actual_received = models.DecimalField(max_digits=10, decimal_places=2)
+    reaming_due = models.DecimalField(max_digits=10, decimal_places=2)
+    due_payable_last_date = models.DateField(
+        blank=True, null=True, default=None)
+
+    # relations
+    particular = models.ForeignKey(
+        IncomeParticular, on_delete=models.PROTECT, related_name="income_particular")
+
+    received_from_type = models.ForeignKey(
+        IncomeReceivingOption, on_delete=models.PROTECT, related_name="income_received_from_type")
+
+    receiving_type = models.ForeignKey(
+        IncomeReceivingType, on_delete=models.PROTECT, related_name="income_receiving_type")
+
+    member = models.ForeignKey(
+        Member, on_delete=models.PROTECT, related_name="income_member")
+    received_by = models.ForeignKey(
+        PaymentMethod, on_delete=models.PROTECT, related_name="income_received_by")
