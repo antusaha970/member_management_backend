992458f41c37dfcd4ca4b44e176d7412cbc015bc
Antu Saha <antusaha970@gmail.com>
2025-04-07 15:11:47 +0600

    added logger in payment method apis

--- member_financial_management/views.py
+++ member_financial_management/views.py
@@ -2,8 +2,12 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+import logging
+from activity_log.tasks import log_activity_task
+from activity_log.utils.functions import request_data_activity_log
 from .models import PaymentMethod
 from . import serializers
+logger = logging.getLogger("myapp")
 
 
 class PaymentMethodView(APIView):
@@ -13,6 +17,12 @@ class PaymentMethodView(APIView):
         try:
             data = PaymentMethod.objects.filter(is_active=True)
             serializer = serializers.PaymentMethodSerializer(data, many=True)
+            log_activity_task.delay_on_commit(
+                request_data_activity_log(request),
+                verb="View",
+                severity_level="info",
+                description="User viewed all payment methods.",
+            )
             return Response({
                 "code": 200,
                 "status": "success",
@@ -20,6 +30,13 @@ class PaymentMethodView(APIView):
                 "data": serializer.data
             }, status=status.HTTP_200_OK)
         except Exception as e:
+            logger.exception(str(e))
+            log_activity_task.delay_on_commit(
+                request_data_activity_log(request),
+                verb="Creation",
+                severity_level="info",
+                description="User tried to view payment method but faced error.",
+            )
             return Response({
                 "code": 500,
                 "status": "failed",
@@ -32,6 +49,12 @@ class PaymentMethodView(APIView):
             serializer = serializers.PaymentMethodSerializer(data=request.data)
             if serializer.is_valid():
                 serializer.save()
+                log_activity_task.delay_on_commit(
+                    request_data_activity_log(request),
+                    verb="Creation",
+                    severity_level="info",
+                    description="User created a new payment method",
+                )
                 return Response({
                     "code": 201,
                     "status": "success",
@@ -39,6 +62,12 @@ class PaymentMethodView(APIView):
                     "data": serializer.data
                 }, status=status.HTTP_201_CREATED)
             else:
+                log_activity_task.delay_on_commit(
+                    request_data_activity_log(request),
+                    verb="Creation",
+                    severity_level="info",
+                    description="User tried to create a new payment method but faced error.",
+                )
                 return Response({
                     "code": 400,
                     "status": "success",
@@ -46,6 +75,13 @@ class PaymentMethodView(APIView):
                     "errors": serializer.errors
                 }, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
+            logger.exception(str(e))
+            log_activity_task.delay_on_commit(
+                request_data_activity_log(request),
+                verb="Creation",
+                severity_level="info",
+                description="User tried to create a new payment method but faced error.",
+            )
             return Response({
                 "code": 500,
                 "status": "success",
