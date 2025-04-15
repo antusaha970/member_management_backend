778dfb001009b3ac89d96602925fa2976f9cc68f
Antu Saha <antusaha970@gmail.com>
2025-04-15 12:11:01 +0600

    made some changes

--- promo_code_app/models.py
+++ promo_code_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from core.models import MembershipType
 from member.models import Member
+from django.utils import timezone
 
 
 class PromoCodeBaseModel(models.Model):
@@ -23,28 +24,41 @@ class PromoCode(PromoCodeBaseModel):
     promo_code = models.CharField(max_length=100, unique=True)
     start_date = models.DateField()
     end_date = models.DateField()
-    percentage = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    percentage = models.DecimalField(
+        max_digits=5, decimal_places=2, null=True, blank=True)
+    amount = models.DecimalField(
+        max_digits=10, decimal_places=2, null=True, blank=True)
     limit = models.IntegerField()
-    remaining_limit = models.IntegerField(blank=True, default=0) 
+    remaining_limit = models.IntegerField(blank=True, default=0)
     description = models.TextField()
     # foreignkey relations
-    category = models.ForeignKey(PromoCodeCategory, on_delete=models.RESTRICT, related_name="promo_code_category")
-    
+    category = models.ForeignKey(
+        PromoCodeCategory, on_delete=models.RESTRICT, related_name="promo_code_category")
+
     def __str__(self):
         return self.promo_code
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:  # Only on creation
+            self.remaining_limit = self.limit
+        super().save(*args, **kwargs)
+
+    def is_promo_code_valid(self):
+        today = timezone.now().date()
+        return (
+            self.start_date <= today <= self.end_date and
+            self.remaining_limit > 0
+        )
 
 
 class AppliedPromoCode(PromoCodeBaseModel):
     discounted_amount = models.DecimalField(max_digits=10, decimal_places=2)
-    used_at = models.DateTimeField(auto_now_add=True) 
+    used_at = models.DateTimeField(auto_now_add=True)
     # foreignkey relations
-    promo_code = models.ForeignKey(PromoCode, on_delete=models.RESTRICT, related_name="applied_promo_code")
-    used_by = models.ForeignKey(Member, on_delete=models.RESTRICT, related_name="applied_promo_code_member")
-    
-    
+    promo_code = models.ForeignKey(
+        PromoCode, on_delete=models.RESTRICT, related_name="applied_promo_code")
+    used_by = models.ForeignKey(
+        Member, on_delete=models.RESTRICT, related_name="applied_promo_code_member")
+
     def __str__(self):
         return f"Promo {self.promo_code.promo_code} used by {self.used_by.member_ID} "
-
-
-
