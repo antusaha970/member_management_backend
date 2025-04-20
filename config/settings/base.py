37ab3bf55b36f3d1711c7dd16c727e3485c44794
Antu Saha <antusaha970@gmail.com>
2025-04-20 13:59:45 +0600

    added new settings for caching

--- config/settings/base.py
+++ config/settings/base.py
@@ -142,6 +142,15 @@ CACHES = {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
         # "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "SERIALIZER": "django_redis.serializers.json.JSONSerializer",
+            "COMPRESSOR": "django_redis.compressors.zlib.ZlibCompressor",
+            "COMPRESSOR_OPTIONS": {
+                "min_length": 1024
+            },
+            "IGNORE_EXCEPTIONS": True,
+        }
     }
 }
 
