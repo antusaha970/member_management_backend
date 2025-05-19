98786554d22ca8e69a7c7ef70ccf50ccc2eab39c
salauddin85 <ahmedsalauddin677785@gmail.com>
2025-05-19 13:36:34 +0600

    added dynamic field in admin

--- core/admin.py
+++ core/admin.py
@@ -1,12 +1,18 @@
-from django.contrib import admin
-from . import models
 
-admin.site.register(models.Gender)
-admin.site.register(models.MembershipType)
-admin.site.register(models.InstituteName)
-admin.site.register(models.MembershipStatusChoice)
-admin.site.register(models.MaritalStatusChoice)
-admin.site.register(models.EmailTypeChoice)
-admin.site.register(models.SpouseStatusChoice)
-admin.site.register(models.DescendantRelationChoice)
-admin.site.register(models.DocumentTypeChoice)
+
+from django.contrib import admin
+from django.apps import apps
+
+app_name = "core"
+models_list = apps.get_app_config(app_name).get_models()
+
+for model in models_list:
+    class DynamicAdmin(admin.ModelAdmin):
+        list_display = [field.name for field in model._meta.fields]
+        
+    try:
+        admin.site.register(model, DynamicAdmin)
+    except admin.sites.AlreadyRegistered:
+        pass
+
+
