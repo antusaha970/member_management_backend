f74993e0893fc76c1a6ae4a876ee20385cf6abf9
Antu Saha <antusaha970@gmail.com>
2025-06-22 12:39:21 +0600

    added endpoint for view all configuration

--- mails/views.py
+++ mails/views.py
@@ -6,6 +6,7 @@ import logging
 from activity_log.tasks import log_activity_task
 from activity_log.utils.functions import request_data_activity_log
 from rest_framework import status
+from .models import SMTPConfiguration
 
 
 logger = logging.getLogger("myapp")
@@ -48,6 +49,36 @@ class SetMailConfigurationAPIView(APIView):
             log_activity_task.delay_on_commit(
                 request_data_activity_log(request),
                 verb="Create",
+                severity_level="info",
+                description="User tried to create a mail configuration but faced an error",
+            )
+            return Response({
+                "code": 500,
+                "status": "failed",
+                "message": "Something went wrong",
+                "errors": {
+                    "server_error": [str(e)]
+                }
+            }, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+    def get(self, request):
+        try:
+            user = request.user
+            configs = SMTPConfiguration.objects.filter(user=user)
+            serializer = serializers.SMTPConfigurationSerializer(
+                configs, many=True)
+            return Response({
+                "code": 200,
+                "status": "success",
+                "message": "Mail configuration retrieved successfully",
+                "data": serializer.data
+            }, status=200)
+
+        except Exception as e:
+            logger.exception(str(e))
+            log_activity_task.delay_on_commit(
+                request_data_activity_log(request),
+                verb="",
                 severity_level="info",
                 description="User tried to create a mail configuration but faced an error",
             )
